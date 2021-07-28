891c329b0261d9ec3cb601b53a9d625eba5d585a
BartVHoey <79847633+BartVHoey@users.noreply.github.com>
2021-07-28 22:02:05 +0200

    addConnectToApi

---
+++ ConnectToApi/ConnectToApi/ConnectToApi/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ConnectToApi
+{
+    public partial class App
+    {
+        public const string AuthorityUrl = "https://618beff66fa6.ngrok.io";
+        public const string CallbackUri = "xamarinformsclients";
+        public static readonly string RedirectUrl = $"{CallbackUri}:/authenticated";
+        public static readonly string PostLogoutRedirectUrl = $"{CallbackUri}:/signout-callback-oidc";
+        public const string Scopes = "email openid profile role phone address Medusa";
+        public const string? ClientSecret = "1q2w3e*";
+        public const string ClientId = "Medusa_Xamarin";
+
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new MainPage();
+        }
+
+
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
